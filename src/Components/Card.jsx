103bfefecb3101fd8f2b95e8318e82c951afd82e
Avik Ghosh <98302991+Avik1320@users.noreply.github.com>
2023-11-14 16:47:38 +0530

    first commit

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card-main'> 
+      Card section
+    </div>
+  )
+}
+
+export default Card
---
+++ src/Pages/Pages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pages = () => {
+  return (
+    <div className='page'>
+      This Is page section.
+      
+    </div>
+  )
+}
+
+export default Pages
